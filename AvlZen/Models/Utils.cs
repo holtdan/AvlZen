4a21c00a22930bc9fec9f226931245200de99a78
Daniel Holt <HoltDan1@gmail.com>
2014-02-07 18:04:15 -0500

    2 new files before version REDO

---
+++ AvlZen/Models/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AvlZen.Models
+{
+    public class Utils
+    {
+        public static string ExcelFileName
+        {
+            get
+            {
+                var dd = AppDomain.CurrentDomain.GetData("DataDirectory");
+                var efn = System.Configuration.ConfigurationManager.AppSettings["ExcelFileName"];
+                return dd + "\\" + efn;
+            }
+        }
+    }
+}
