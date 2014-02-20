5b74b0e3ed5c2c884d3faf1930b0d4c48379d952
Daniel Holt <HoltDan1@gmail.com>
2014-02-20 15:57:28 -0500

    Adds contact form and new info email

--- AvlZen/Controllers/HomeController.cs
+++ AvlZen/Controllers/HomeController.cs
@@ -1,8 +1,8 @@
-﻿using AvlZen.Model;
-using AvlZen.Models;
+﻿using AvlZen.Models;
 using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -45,10 +45,54 @@ namespace AvlZen.Controllers
         public ActionResult Contact()
         {
             //ViewBag.Message = "Your contact page.";
-            var vm = new PlacesModel();
+            var vm = new ContactFormModel();
             vm.Init();
 
             return View(vm);
+        }
+        [HttpPost]
+        public ActionResult Contact(ContactFormModel model)
+        {
+            //string Text = "<html> <head> </head>" +
+            //" <body style= \" font-size:12px; font-family: Arial\">" +
+            //Model.Message +
+            //"</body></html>";
+
+            SendEmail(model);
+
+            var vm = new ContactFormModel();
+            vm.Init();
+            ModelState.Clear();
+            return View(vm);
+        }
+        public static bool SendEmail(ContactFormModel model)
+        {
+            MailMessage msg = new MailMessage();
+
+            msg.From = new MailAddress("info@avlzen.com");
+            msg.To.Add(new MailAddress("info@avlzen.com"));
+            msg.Subject = "AVL Zen Contact";
+            msg.Body = string.Format("{0},{1}\n{2}", model.Name, model.Email, model.Message);
+            msg.Priority = MailPriority.Normal;
+
+            var client = new SmtpClient("mail.avlzen.com");
+            
+
+            client.UseDefaultCredentials = false;
+            //client.EnableSsl = false;
+            client.Credentials = new NetworkCredential("info@avlzen.com", "avzinfo14");
+            //client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            //client.EnableSsl = true;
+
+            try
+            {
+                client.Send(msg);
+            }
+            catch (Exception exc)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
---
+++ AvlZen/Models/ContactFormModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AvlZen.Models
+{
+    public class ContactFormModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+        public PlacesModel PlacesModel { get; set; }
+
+        public void Init()
+        {
+            PlacesModel = new PlacesModel();
+            PlacesModel.Init();
+        }
+    }
+}
