33f9ab56fda611655e0508b517826082821f8430
Daniel Holt <HoltDan1@gmail.com>
2014-02-23 13:07:39 -0500

    Formatting, Google Maps & Calendar play

---
+++ AvlZen/Controllers/EventsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AvlZen.Controllers
+{
+    public class EventsController : Controller
+    {
+        //
+        // GET: /Events/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
